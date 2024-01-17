f5fe394509c2251e60d6572867b2ef0b35243645
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2024-01-17 23:36:21 +0500

    feat: easy solutions

---
+++ Easy/1201-1300/1207. Unique Number of Occurrences.py
@@ -0,0 +1,44 @@
+# 1207. Unique Number of Occurrences
+# 
+# Given an array of integers arr, return true if the number of occurrences of each value in the array is 
+# unique or false otherwise.
+# 
+# Example 1:
+# Input: arr = [1,2,2,1,1,3]
+# Output: true
+# Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+# 
+# Example 2:
+# Input: arr = [1,2]
+# Output: false
+# 
+# Example 3:
+# Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+# Output: true
+# 
+# Constraints:
+# 
+# 1 <= arr.length <= 1000
+# -1000 <= arr[i] <= 1000
+
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        occurrences = {}
+        unique = set()
+        for i in arr:
+            if i in occurrences:
+                occurrences[i] += 1
+            else:
+                occurrences[i] = 1
+        for j in occurrences.values():
+            if j in unique:
+                return False
+            unique.add(j)
+        return True
+
+# Tasks are the property of LeetCode (https://leetcode.com/) 
+# and users of this resource.
+# 
+# All solution code in this repository 
+# is the personal property of Vladimir Rukavishnikov
+# (vladimirrukavishnikovmail@gmail.com).
