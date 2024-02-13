2a749da0ff5c46da1c9c1cfd6f312fb38cb62054
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2024-02-13 22:17:29 +0500

    feat: easy solutions

---
+++ Easy/2101-2200/2108. Find First Palindromic String in the Array.py
@@ -0,0 +1,59 @@
+# 2108. Find First Palindromic String in the Array
+# 
+# Given an array of strings words, return the first palindromic string in the array. If there is no 
+# such string, return an empty string "".
+# 
+# A string is palindromic if it reads the same forward and backward.
+# 
+# Example 1:
+# Input: words = ["abc","car","ada","racecar","cool"]
+# Output: "ada"
+# Explanation: The first string that is palindromic is "ada".
+# Note that "racecar" is also palindromic, but it is not the first.
+# 
+# Example 2:
+# Input: words = ["notapalindrome","racecar"]
+# Output: "racecar"
+# Explanation: The first and only string that is palindromic is 
+# "racecar".
+# 
+# Example 3:
+# Input: words = ["def","ghi"]
+# Output: ""
+# Explanation: There are no palindromic strings, so the empty 
+# string is returned.
+# 
+# Constraints:
+# 
+# 1 <= words.length <= 100
+# 1 <= words[i].length <= 100
+# words[i] consists only of lowercase English letters.
+
+class Solution:
+    def firstPalindrome(self, words: List[str]) -> str:
+        for i in words:
+            if i == i[::-1]:
+                return i
+        return ""
+
+# or
+
+class Solution:
+    def firstPalindrome(self, words: List[str]) -> str:
+        for i in words:
+            left, right = 0, len(i) - 1
+            while left < right:
+                if i[left] != i[right]:
+                    break
+                left += 1
+                right -= 1
+            if left >= right:
+                return i
+        return ""
+
+# Tasks are the property of LeetCode (https://leetcode.com/) 
+# and users of this resource.
+# 
+# All solution code in this repository 
+# is the personal property of Vladimir Rukavishnikov
+# (vladimirrukavishnikovmail@gmail.com).
