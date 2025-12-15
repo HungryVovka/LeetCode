6f4f096facb1aa7b020085dbbf0f92bc7911f670
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2025-12-15 18:56:00 +0100

    feat: medium solutions

--- Medium/1-100/2. Add Two Numbers.c
+++ Medium/1-100/2. Add Two Numbers.c
@@ -74,6 +74,48 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
     return dummy_head->next;
 }
 
+// or
+
+#include <stdlib.h>     // malloc, free
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
+    struct ListNode* dummy = (struct ListNode *)malloc(sizeof(struct ListNode));
+    struct ListNode* tail = dummy;
+    int carry = 0;
+    dummy->val = 0;
+    dummy->next = NULL;
+    while (l1 != NULL || l2 != NULL || carry != 0){
+        int temp_sum = carry;
+
+        if (l1 != NULL){
+            temp_sum += l1->val;
+            l1 = l1->next;
+        }
+        if (l2 != NULL){
+            temp_sum += l2->val;
+            l2 = l2->next;
+        }
+        carry = temp_sum >= 10;
+        temp_sum = carry ? (temp_sum - 10) : temp_sum;
+        tail->next = (struct ListNode *)malloc(sizeof(struct ListNode));
+        tail = tail->next;
+        tail->val = temp_sum;
+        tail->next = NULL;
+    }
+    struct ListNode* answer = dummy->next;
+    free(dummy);
+    return answer;
+}
+
 // Tasks are the property of LeetCode (https://leetcode.com/) 
 // and users of this resource.
 // 
